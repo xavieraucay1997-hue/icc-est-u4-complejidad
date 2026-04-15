f9702a4c0ef7de61bfed8cc5bedab870c6aa7537
xavieraucay1997-hue <xavieraucay1997@gmail.com>
2026-04-14 19:50:11 -0500

    Add array initialization and print elements
    Replaced 'Hello, World!' with array initialization and printing.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] array = new int[]{1,2,3,4,5,6,}; 
+        System.out.println(array[0]);
+        System.out.println(array);
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+        for (int i : array) {
+            System.out.println(i);
+        }
     }
 }
